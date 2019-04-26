d139f8f6bb9e438e21a5b95fa426b484dd087d7f
DataUrbanEconGeek <andrew.a.rosa@gmail.com>
2019-04-26 12:50:20 +0000

    Add richmond gentrification map

--- code/raw_code/14_census-data.R
+++ code/raw_code/14_census-data.R
@@ -197,12 +197,14 @@ rva_tracts <- fortify(rva_tracts, region = "TRACTCE")
 rva_tracts_n_gent <- rva_tracts %>%
   inner_join(rva_gent, by = c("id" = "tract"))
   
-ggplot(rva_tracts_n_gent, aes(x = long, y = lat, group = group)) +
+gent_map <- ggplot(rva_tracts_n_gent, aes(x = long, y = lat, group = group)) +
   geom_polygon(aes(fill = gentrified, color = gentrified), alpha = 0.5) +
   theme_minimal() +
   theme(axis.text = element_blank(), axis.title = element_blank(),
         panel.grid = element_blank())
   
+dest_path <- "../../figures/exploratory_figures/gentried_map.png"
+ggsave(filename = dest_path, gent_map)
 
 
 
